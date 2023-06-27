ac377b09fae9ef07dce6cd1a6eeadfdae695560a
Vetha <vetha.vinnarasu@ippopay.com>
2023-06-27 15:03:28 +0530

    0.0.1 version updated

---
+++ Tutelar.xcframework/ios-arm64/Tutelar.framework/Headers/Tutelar.h
@@ -0,0 +1,18 @@
+//
+//  Tutelar.h
+//  Tutelar
+//
+//  Created by Admin on 20/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tutelar.
+FOUNDATION_EXPORT double TutelarVersionNumber;
+
+//! Project version string for Tutelar.
+FOUNDATION_EXPORT const unsigned char TutelarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tutelar/PublicHeader.h>
+
+
